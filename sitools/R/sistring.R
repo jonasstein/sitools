999cc49bac7fb4c4fd46a0e562633f7873e61787
Jonas Stein <news@jonasstein.de>
2012-01-10 23:13:28 +0100

    function and skeleton included

---
+++ sitools/R/sistring.R
@@ -0,0 +1,26 @@
+sistring <-
+function(number){
+
+  lut <-  c(1e-24, 1e-21, 1e-18, 1e-15,
+            1e-12, 1e-09, 1e-06, 1e-03,
+            1e+00,
+            1e+03, 1e+06, 1e+09, 1e+12,
+            1e+15, 1e+18, 1e+21, 1e+24)
+
+  pre <- c("y", "z", "a", "f",
+           "p", "n", "u", "m",
+           " ",
+           "k", "M", "G", "T",
+           "P", "E", "Z", "Y")
+
+  ix <- findInterval(number, lut)
+ 
+  if (length(ix) > 0 ) {
+    sistring <- paste(number/lut[ix], pre[ix])
+  } else {
+    sistring <- as.character(x)
+  }
+
+  return(sistring)
+}
+
