04181b69253fa884e5832275cf176de8e81f5412
Jonas Stein <news@jonasstein.de>
2012-01-18 23:42:11 +0100

    all prefixes added as constants

--- init.R
+++ init.R
@@ -26,6 +26,28 @@ sistring <- function(number){
 
 package.skeleton(list=c("sistring"), name="sitools")
 
+yotta <- 1e24
+zetta <- 1e21
+exa <- 1e18
+peta <- 1e15
+tera <- 1e12
+giga <- 1e9
+mega <- 1e6
 kilo <- 1e3
+hecto <- 1e2
+deca <- 1e1
+deci <- 1e-1
+centi <- 1e-2
 milli <- 1e-3
-save(file="siprefixvalues.rda", list=c("kilo","milli"))
+micro <- 1e-6
+nano <- 1e-9
+pico <- 1e-12
+femto <- 1e-15
+atto <- 1e-18
+zepto <- 1e-21
+yocto <- 1e-24
+
+
+save(file="siprefixvalues.rda",
+     list=c("yotta", "zetta", "exa", "peta", "tera", "giga", "mega", "kilo", "hecto", "deca",
+       "deci", "centi", "milli", "micro", "nano", "pico", "femto", "atto", "zepto", "yocto"))
