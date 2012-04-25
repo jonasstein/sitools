ea42d415f04a95e6059b098277a0182a6829b966
Jonas Stein <news@jonasstein.de>
2012-04-25 23:46:21 +0200

    add unit and sep

--- sitools/R/f2si.R
+++ sitools/R/f2si.R
@@ -1,5 +1,5 @@
 f2si <-
-function(number){
+function(number, sep=" ", unit=""){
 
   lut <-  c(1e-24, 1e-21, 1e-18, 1e-15,
             1e-12, 1e-09, 1e-06, 1e-03,
@@ -9,18 +9,23 @@ function(number){
 
   pre <- c("y", "z", "a", "f",
            "p", "n", "u", "m",
-           " ",
+           "",
            "k", "M", "G", "T",
            "P", "E", "Z", "Y")
 
   ix <- findInterval(number, lut)
  
   if (length(ix) > 0 ) {
-    sistring <- paste(number/lut[ix], pre[ix])
+    sistring <- paste(number/lut[ix], pre[ix], sep=sep, unit=unit)
   } else {
     sistring <- as.character(number)
   }
 
   return(sistring)
 }
+
+# test vector
+# a <- 2.34*10^(-22:22)
+# b <-  2.34*10^((-8:8)*3)
+# c <-  -2.34*10^((-8:8)*3)
 
