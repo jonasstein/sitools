4b65d2f2e6621f71cbaa416d0ff22288a42eea10
Jonas Stein <news@jonasstein.de>
2012-08-21 11:27:49 +0200

    thanks to Roland Fuß added

--- Makefile
+++ Makefile
@@ -17,3 +17,4 @@ clean :
 
 purge : clean
 	rm -f  $(packagename)*.tar.gz
+
--- sitools/R/f2si.R
+++ sitools/R/f2si.R
@@ -1,11 +1,13 @@
 f2si <-
 function(number, sep=" ", unit=""){
 
-  lut <-  c(1e-24, 1e-21, 1e-18, 1e-15,
-            1e-12, 1e-09, 1e-06, 1e-03,
-            1e+00,
-            1e+03, 1e+06, 1e+09, 1e+12,
-            1e+15, 1e+18, 1e+21, 1e+24)
+  seperator <- (" ")
+  unit <- (" ")
+  sifactor <-  c(1e-24, 1e-21, 1e-18, 1e-15,
+                 1e-12, 1e-09, 1e-06, 1e-03,
+                 1e+00,
+                 1e+03, 1e+06, 1e+09, 1e+12,
+                 1e+15, 1e+18, 1e+21, 1e+24)
 
   pre <- c("y", "z", "a", "f",
            "p", "n", "u", "m",
@@ -13,10 +15,13 @@ function(number, sep=" ", unit=""){
            "k", "M", "G", "T",
            "P", "E", "Z", "Y")
 
-  ix <- findInterval(number, lut)
- 
+
+  absolutenumber <- number * sign(number)
+  ix <- findInterval(absolutenumber, sifactor)
+
+  
   if (length(ix) > 0 ) {
-    sistring <- paste(number/lut[ix], pre[ix], sep=sep, unit=unit)
+    sistring <- paste(absolutenumber/sifactor[ix], seperator, pre[ix], sep="", unit=unit)
   } else {
     sistring <- as.character(number)
   }
@@ -24,8 +29,12 @@ function(number, sep=" ", unit=""){
   return(sistring)
 }
 
+fl2si <- function(floatlist, sep=" ", unit=""){
+  return(sapply(floatlist, f2si))
+}
+
 # test vector
-# a <- 2.34*10^(-22:22)
-# b <-  2.34*10^((-8:8)*3)
-# c <-  -2.34*10^((-8:8)*3)
+#a <- 2.34*10^(-22:22)
+#b <-  2.34*10^((-8:8)*3)
+#c <-  -2.34*10^((-8:8)*3)
 
