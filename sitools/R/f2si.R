3ae0aa5d75bf577ebe1846e2646447cbce375636
Jonas Stein <news@jonasstein.de>
2012-08-21 12:56:26 +0200

    fixed space bug

--- sitools/R/f2si.R
+++ sitools/R/f2si.R
@@ -1,31 +1,29 @@
 f2si <-
-function(number, sep=" ", unit=""){
+function(number, unit=""){
 
-  seperator <- (" ")
-  unit <- (" ")
   sifactor <-  c(1e-24, 1e-21, 1e-18, 1e-15,
                  1e-12, 1e-09, 1e-06, 1e-03,
                  1e+00,
                  1e+03, 1e+06, 1e+09, 1e+12,
                  1e+15, 1e+18, 1e+21, 1e+24)
 
-  pre <- c("y", "z", "a", "f",
-           "p", "n", "u", "m",
+  pre <- c(" y", " z", " a", " f",
+           " p", " n", " u", " m",
            "",
-           "k", "M", "G", "T",
-           "P", "E", "Z", "Y")
-
-
-  absolutenumber <- number * sign(number)
-  ix <- findInterval(absolutenumber, sifactor)
+           " k", " M", " G", " T",
+           " P", " E", " Z", " Y")
 
   
+  
+  absolutenumber <- number * sign(number)      # findInterval should get positive numbers
+                                        # we will fix the sign later again
+  ix <- findInterval(absolutenumber, sifactor) # ix is a list like (1 0 0 1 1) 
+  
   if (length(ix) > 0 ) {
-    sistring <- paste(absolutenumber/sifactor[ix], seperator, pre[ix], sep="", unit=unit)
+    sistring <- paste(number/sifactor[ix], pre[ix], sep="", unit=unit)
   } else {
     sistring <- as.character(number)
   }
-
   return(sistring)
 }
 
